0feeeda41e2dbe7726e6c5e410e94ad8a39ee95c
eventjango <eventjango@gmail.com>
2018-01-30 17:02:40 +0900

    sdfdsfdf

--- src/main/java/com/example/demo/test/UserController.java
+++ src/main/java/com/example/demo/test/UserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.test;
 
 
+import com.google.gson.JsonObject;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -8,16 +9,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 
 @Slf4j
 @Controller
@@ -44,7 +51,7 @@ public class UserController {
 
 
     @GetMapping("/callback")
-    public String callback(HttpServletRequest request) throws IOException, JSONException{
+    public String callback(HttpServletRequest request, Model model) throws IOException, JSONException{
 
         String errorCode = request.getParameter("error");
 
@@ -98,10 +105,35 @@ public class UserController {
             }
         };
 
-        Map.Entry<String, Object> header =
 
-        headerMap.entrySet()
-                .stream().findFirst().get();
+        Set<Map.Entry<String, Object>> headerSet = headerMap.entrySet();
+        Map.Entry<String, Object> header = headerSet.stream().findFirst().get();
+
+        String api = "http://localhost:80";
+        api += "/api/profile";
+
+        HttpURLConnection urlConnection = (HttpURLConnection) new URL(api).openConnection();
+
+        urlConnection.setRequestMethod("GET");
+        urlConnection.setRequestProperty(header.getKey(), header.getValue().toString());
+
+        int responseCode = urlConnection.getResponseCode();
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(responseCode == 200? urlConnection.getInputStream() : urlConnection.getErrorStream()));
+
+        String line;
+        StringBuffer stringBuffer = new StringBuffer();
+
+        while ((line = in.readLine()) != null){
+            stringBuffer.append(line);
+        }
+
+        in.close();
+
+        JSONObject json = new JSONObject(stringBuffer.toString());
+        model.addAttribute("user", json.toString());
+
+        log.debug(json.toString());
 
         return "";
 
