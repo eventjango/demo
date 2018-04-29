5fa10f91630ffbf81b662a0833ab38630c76b5a5
eventjango <eventjango@gmail.com>
2018-04-29 21:48:19 +0900

    sfdsf

--- src/test/java/com/example/demo/dummy/StreamExample.java
+++ src/test/java/com/example/demo/dummy/StreamExample.java
@@ -1,4 +1,31 @@
 package com.example.demo.dummy;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class StreamExample {
+
+
+    /**
+     * Stream.of(T... values) : Stream<T>
+     */
+    @Test
+    public void create(){
+
+        System.out.println("count : " + Stream.of("kevin", "jack", "may").count());
+        System.out.println("count : " + Stream.of(Arrays.asList("kevin", "jack", "may")).count());
+
+        Stream.of(Arrays.asList("kevin", "jack", "may"))
+                .forEach(
+                        list ->
+                        {
+                            list.stream()
+                                    .forEach(n -> System.out.println(n));
+                        }
+                );
+    }
 }
+
+
