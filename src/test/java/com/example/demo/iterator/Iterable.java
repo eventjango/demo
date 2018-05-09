ca278f34c1620cdf2af3d725636e3f6dbe9adc9f
eventjango <eventjango@gmail.com>
2018-05-09 15:33:13 +0900

    fuck

--- src/test/java/com/example/demo/iterator/Iterable.java
+++ src/test/java/com/example/demo/iterator/Iterable.java
@@ -1,6 +1,14 @@
 package com.example.demo.iterator;
 
+
+/**
+ * 이터레이터를 생성하는 역할
+ */
 public interface Iterable {
 
+    /**
+     * 이터레이터를 생성하는 책임
+     * @return
+     */
     Iterator iterator();
 }
--- src/test/java/com/example/demo/iterator/Iterator.java
+++ src/test/java/com/example/demo/iterator/Iterator.java
@@ -1,8 +1,21 @@
 package com.example.demo.iterator;
 
+
+/**
+ * Iterable에 접근해서 요소를 순회하는 역할
+ */
 public interface Iterator {
 
+
+    /**
+     * 다음 요소의 존재여부를 확인하는 책임
+     * @return
+     */
     boolean hasNext();
 
+    /**
+     * 다음 요소를 리턴하며 시퀀싱하는 책임
+     * @return
+     */
     Object next();
 }
